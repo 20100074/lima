9ce82d63b4060cdd100cb56c205bb8415ddf09f3
KOSHIKAWA Kenichi <reishoku.misc@pm.me>
2022-02-01 22:00:46 +0900

    Parse DNS Configuration

--- pkg/osutil/dns_others.go
+++ pkg/osutil/dns_others.go
@@ -3,8 +3,42 @@
 
 package osutil
 
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "strings"
+)
+
+func ReadDNSSettings() (filename string) {
+  // cat /run/systemd/resolve/resolv.conf
+
+  var availableDNS []string
+  file, err := os.Open(filename)
+  if err != nil {
+    return err.s();
+  }
+
+  s := bufio.NewScanner(file)
+  for s.Scan() {
+    // if line contains nameserver,
+    if strings.Contains(s.Text(), 'nameserver') {
+      // each line contains DNS address
+      // FIXME: fmt.Printf doesn't return string
+      // availableDNS = append(fmt.Printf("%s", strings.TrimLeft(s.Text(), "nameserver ")), availableDNS)
+      availableDNS := append(availableDNS, strings.TrimLeft(s.Text(), "nameserver "))
+    }
+  }
+  if s.Err() != nil {
+    return s.Err().Text()
+  }
+
+  return availableDNS
+}
+
 func DNSAddresses() ([]string, error) {
 	// TODO: parse /etc/resolv.conf?
+	// cat /run/systemd/resolve/resolv.conf | grep nameserver | gawk '{print $2}'
 	return []string{}, nil
 }
 
